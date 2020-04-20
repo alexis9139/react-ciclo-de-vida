5fe8cf188fa04ced87aae7d77e36ce433b864284
Medrano Alexis Bruno Baloy <alexismedrano9139@gmail.com>
2020-04-20 12:54:41 -0300

    6. El método getSnapshotBeforeUpdate 2

--- src/getSnapshotBeforeUpdate.js
+++ src/getSnapshotBeforeUpdate.js
@@ -1,37 +1,95 @@
 import React from 'react'
+import faker from 'faker'
 
+const chatStyle = {
+    width: 230,
+    height: 300,
+    border: '1px solid gray',
+    overflow: 'auto',
+    fontFamily: 'monospace'
+}
+
+const messageStyle = {
+    padding: '1em',
+    borderBottom: '1px solid #DDD'
+}
+
+const avatarStyle = {
+    width: 50,
+    height: 50,
+    borderRadius: '50%'
+}
+
+class Chat extends React.Component {
+
+    box = React.createRef()
+
+    getSnapshotBeforeUpdate() {
+        const box = this.box.current
+        if (box.scrollTop + box.offsetHeight >= box.scrollHeight) {
+            return true
+        }
+
+        return false
+    }
+
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        const box = this.box.current
+
+        if (snapshot) {
+            box.scrollTop = box.scrollHeight
+        }
+
+    }
+
+    render() {
+        return (
+            <div style={chatStyle} ref={this.box}>
+                {this.props.list.map(item => (
+                    <div key={item.id} style={messageStyle}>
+                        <p>{item.mensaje}</p>
+                        <div>
+                            {item.name}
+                        </div>
+                        <img src={item.avatar} style={avatarStyle}></img>
+                    </div>
+                ))}
+            </div>
+        )
+    }
+}
 
 
 class App extends React.Component {
-
-    title = React.createRef()
-
     state = {
-        text: 'hola'
+        list: [{}]
     }
 
-    getSnapshotBeforeUpdate(prevProps, prevState) {
-        console.log("Desde getSnapshotBeforeUpdate " + this.title.current.innerHTML)
-        return "Adios desde getSnapshotBeforeUpdate"
-    }
-
-    componentDidUpdate(prevProps, prevState, snapshot) {
-        console.log("Desde componentDidUpdate " + this.title.current.innerHTML)
-        console.log(snapshot)
-    }
-
-
-    enviar = () => {
-        this.setState({
-            text: 'Adios'
+    agregarMensaje = () => {
+        //crear un mensaje falso, mensaje es un objeto
+        const mensaje = {
+            id: faker.random.uuid(),
+            name: faker.name.findName(),
+            avatar: faker.image.avatar(),
+            mensaje: faker.hacker.phrase()
+        }
+        //agregarlo a la lista
+        this.setState(state => ({
+            list: [
+                ...state.list,
+                mensaje
+            ]
+        }), () => {
+            console.log(this.state.list)
         })
     }
 
     render() {
         return (
             <div>
-                <h1 ref={this.title}>{this.state.text}</h1>
-                <button onClick={this.enviar}>Cambiar</button>
+                <Chat list={this.state.list}></Chat>
+                <button onClick={this.agregarMensaje}>Agregar mensaje</button>
             </div>
         )
     }
